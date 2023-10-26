641c06819d7faa1f902d973bd7b4a64cc19ebacf
Mike Groves <mike.groves89@outlook.com>
2023-10-26 21:49:05 +0100

    6

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 import './App.css';
-import Login from './pages/login';
-import { HashRouter,Routes, Route } from "react-router-dom";
+//import Login from './pages/login';
+//import { HashRouter,Routes, Route } from "react-router-dom";
+import { Route, Switch, Link } from "react-router-dom";
+import Home from "./pages/home"
+import Login from "./pages/login"
+
 
 function App() {
   return (
-    <HashRouter basename="/app">
-    <Routes>
-      <Route path="/testclient2" /> {Login}
-    </Routes>
-  </HashRouter>
+    <div className="App">
+          <div>
+            <nav>
+              <ul id="navigation">
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                <Link to="/about">About</Link>
+                </li>
+                <li>
+                <Link to="/contact">Contact</Link>
+                </li>
+              </ul>
+            </nav>
+          </div>
+            <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <Login />
+            </Route>
+          </Switch>
+          </div>
   );
 }
 
